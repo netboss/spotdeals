8e96db7eda8561691e1d978bd3237730946a69d9
Ivan <igl67@yahoo.com>
2026-03-14 15:19:46 -0400

    SD-000: Initial local setup

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = 'dd9ef36af7e2c207';
+  public const string VERSIONS_HASH = 'ef188f6d8d394b44';
 
 }
--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -872,3 +872,9 @@ $databases['default']['default'] = array (
   'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
 );
 $settings['config_sync_directory'] = 'sites/default/files/config_hc4q2cI8a_cmh4CsdE6LkqRIY2jgachOtaGuXT9NfLtR48-2Hi9AJc4kzIDKEJdWm23BwlmJ6A/sync';
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = __DIR__ . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
